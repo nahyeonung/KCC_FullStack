27d0ba9b6de644dada772d818918748799ff64d5
ntweety <ntweety@naver.com>
2023-05-18 21:20:29 +0900

    05-18 sql

--- sql/MiniProject_db.sql
+++ sql/MiniProject_db.sql
@@ -2,13 +2,14 @@ CREATE SEQUENCE customer_seq;
 CREATE SEQUENCE product_seq;
 CREATE SEQUENCE order_seq;
 CREATE TABLE customer (
-    customer_id             NUMBER(5)       CONSTRAINT customer_id_pk           PRIMARY KEY,
+    customer_id             VARCHAR2(20)      CONSTRAINT customer_id_pk         PRIMARY KEY,
     customer_name           VARCHAR2(20)    CONSTRAINT customer_name_nn         NOT NULL,
     customer_birthdate      DATE            CONSTRAINT customer_dirthdate_nn    NOT NULL,
     customer_address        VARCHAR2(50)    CONSTRAINT customer_address_nn      NOT NULL,
     customer_phonenumber    VARCHAR2(15)    CONSTRAINT customer_phone_nn        NOT NULL,
     is_admin                NUMBER(1)       DEFAULT 0   CONSTRAINT customer_is_admin_nn NOT NULL,
-    customer_point          NUMBER          DEFAULT 0   CONSTRAINT customer_point_nn    NOT NULL
+    customer_point          NUMBER          DEFAULT 0   CONSTRAINT customer_point_nn    NOT NULL,
+    password                VARCHAR2(20)    CONSTRAINT password                 NOT NULL
 );
 INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point)
 values (customer_seq.nextval,'엄준식', '1996-05-15', 'asdasdasdad', '010-2376-7945',0,0);
@@ -28,20 +29,22 @@ CREATE TABLE order_list (
     delivery_status  VARCHAR2(20)    CONSTRAINT order_delivery_nn    NOT NULL
 );
 
---INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point)
---values (customer_seq.nextval,'고지원', '1994-01-15', '대전 은행구', '010-4526-1998',0,0);
---INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point)
---values (customer_seq.nextval,'배종식', '2020-06-26', '천안 두정동', '010-1880-5288',0,550);
---INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point)
---values (customer_seq.nextval,'신은비', '1988-11-25', '서울 종로구', '010-9535-8589',0,300);
---INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point)
---values (customer_seq.nextval,'김진우', '1979-05-01', '대구 달성구', '010-5658-5515',0,30);
---INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point)
---values (customer_seq.nextval,'admin', '1999-12-31', 'admin', 'admin',1,0);
---INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point)
---values (customer_seq.nextval,'조성호', '1964-08-10', '대전 서구', '010-4798-2118',0,0);
---INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point)
---values (customer_seq.nextval,'이연아', '1990-04-03', '수원시 망포동', '010-5521-6398',0,2);
+--INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point, password)
+--values ('qtqt99','엄준식', '1996-05-15', '서울 금천구', '010-2376-7945',0,0,'qocn0430');
+--INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point, password)
+--values ('hotguy123','고지원', '1994-01-15', '대전 은행구', '010-4526-1998',0,0,'guyontop');
+--INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point, password)
+--values ('hypeboy5q','배종식', '2020-06-26', '천안 두정동', '010-1880-5288',0,550,'06261qq');
+--INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point, password)
+--values ('bebe11your','신은비', '1988-11-25', '서울 종로구', '010-9535-8589',0,300,'123456');
+--INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point, password)
+--values ('ffjdgsl','김진우', '1979-05-01', '대구 달성구', '010-5658-5515',0,30,'wgwg11rr');
+--INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point, password)
+--values ('admin','admin', '1999-12-31', 'admin', 'admin',1,0,'admin123');
+--INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point, password)
+--values ('jiguem2ya','조성호', '1964-08-10', '대전 서구', '010-4798-2118',0,0,'tqtqtq5');
+--INSERT INTO customer (customer_id,customer_name,customer_birthdate,customer_address, customer_phonenumber, is_admin, customer_point, password)
+--values ('yyyaaah123','이연아', '1990-04-03', '수원시 망포동', '010-5521-6398',0,2,'lee1');
 
 --INSERT INTO product (product_id,product_name,product_price,product_stock)
 --values (product_seq.nextval,'브로콜리', 1500, 15);
@@ -81,4 +84,18 @@ CREATE TABLE order_detail (
     product_id      NUMBER(5)       REFERENCES product (product_id),
     product_count   NUMBER          NOT NULL
 );
-commit;
+--구매하기 쿼리문
+--장바구니에 담긴 내용 가져오기
+--select * from cart where customer_id = ?
+--전체상품 재고 - 장바구니에 담긴 각 상품들 개수 update
+--update product set product_stock = product_stock - ? where product_id = ?
+--장바구니에 담긴 상품 가격 * 개수를 한 총 금액 구하기
+--select sum(price) as sprice from (select p.product_price * c.product_count as price from cart c join product p on c.product_id = p.product_id) where customer_id = ?
+--order_list에 주문 삽입
+--insert into order_list (order_id, customer_id, order_date, total_price, delivery_status) values(order_seq.nextval,?,?,?,?)
+--최근에 삽입된 order_id 가져오기
+--select order_id from (select * from order_list order by order_id desc) where rownum = 1
+--order_detail에 값 삽입
+--insert into order_detail (order_id, customer_id, product_id, product_count values(?,?,?,?)
+--customer 테이블에 적립금 update
+--update customer set customer_point=? * 0.95 where customer_id=?
