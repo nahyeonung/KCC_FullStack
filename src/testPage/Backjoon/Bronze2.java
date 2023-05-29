96079d00d02214183988c5d8b7854ae4e6cf097c
ntweety <ntweety@naver.com>
2023-05-29 16:51:09 +0900

    리뷰 분리

--- src/testPage/Backjoon/Bronze2.java
+++ src/testPage/Backjoon/Bronze2.java
@@ -1,24 +1,42 @@
 package testPage.Backjoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.StringTokenizer;
-
-import javax.swing.event.SwingPropertyChangeSupport;
+import java.util.Set;
 
 public class Bronze2 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        String str;
-        while((str = br.readLine()) != null){
-            System.out.println(str);
-        }
-        // bw.close();
-    }    
+		//리스트는 배열과 다르게 인덱스에 값을 넣으면 덮어 씌워지지 않고 기존 값이 뒤로 밀려 남.
+		// ArrayList list = new ArrayList();
+		// list.add(0, 1);
+		// list.add(0, 2);
+		// System.out.println(list);
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextInt();
+		long b = sc.nextInt();
+		long num1 = Math.max(a,b);
+		long num2 = Math.min(a,b);
+		long result = lcd(num1, num2);
+		System.out.println(result);
+
+	}
+	public static long gcd(long num1,long num2){
+		while(num2 > 0){
+			long tmp = num2;
+			num2 = num1 % num2;
+			num1 = tmp;
+		}
+		return num1;
+	}
+
+	public static long lcd(long num1, long num2){
+		return num1 * num2 / gcd(num1, num2);
+	}
 }
--- src/testPage/Backjoon/Dial.java
+++ src/testPage/Backjoon/Dial.java
@@ -1,30 +1,105 @@
 package testPage.Backjoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Dial {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int loop = scan.nextInt();
-        int maximum = scan.nextInt();
-        int min = Integer.MAX_VALUE;
-        int[] arr = new int[loop];
-        for(int i=0; i<loop; i++){
-            arr[i] = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int size = br.read() - '0';
+        br.read();
+        int num = br.read() - '0';
+        br.readLine();
+        int[] arr = new int[size];
+        for(int i=0; i<size; i++){
+            arr[i] = br.read() - '0';
+            br.read();
         }
-        for(int i=0; i<arr.length; i++){
-            for(int j=i+1; j<arr.length; j++){
-                for(int k=j+1; k<arr.length; k++){
-                    int sum = arr[i] + arr[j] + arr[k];
-                    if(sum <= maximum){
-                        if(min >= maximum - sum){
-                            min = maximum - sum;
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println(maximum - min);
+        // System.out.println(size + " | " + num);
+        quickSort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+        
     }
+    public static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+      }
+    
+      private static void quickSort(int[] arr, int start, int end) {
+        // start가 end보다 크거나 같다면 정렬할 원소가 1개 이하이므로 정렬하지 않고 return
+        if (start >= end)
+          return;
+        
+        // 가장 왼쪽의 값을 pivot으로 지정, 실제 비교 검사는 start+1 부터 시작
+        int pivot = start;
+        int lo = start + 1;
+        int hi = end;
+        
+        // lo는 현재 부분배열의 왼쪽, hi는 오른쪽을 의미
+        // 서로 엇갈리게 될 경우 while문 종료
+        while (lo <= hi) {
+          while (lo <= end && arr[lo] <= arr[pivot]) // 피벗보다 큰 값을 만날 때까지
+            lo++;
+          while (hi > start && arr[hi] >= arr[pivot]) // 피벗보다 작은 값을 만날 때까지
+            hi--;
+          if (lo > hi)				 // 엇갈리면 피벗과 교체
+            swap(arr, hi, pivot);
+          else
+            swap(arr, lo, hi);			 // 엇갈리지 않으면 lo, hi 값 교체 
+          }
+        
+        // 엇갈렸을 경우, 
+        // 피벗값과 hi값을 교체한 후 해당 피벗을 기준으로 앞 뒤로 배열을 분할하여 정렬 진행
+        quickSort(arr, start, hi - 1);
+        quickSort(arr, hi + 1, end);
+    
+      }
+    
+      private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    // public static void quick_sort(int[] arr, int start, int end){
+    //     if(start >= end){
+    //         System.out.println(start + "| " + end);
+    //         return;
+    //     }
+    //     int key = start;
+    //     int i = start + 1;
+    //     int j = end;
+
+    //     while (i < j){
+    //         while(arr[j] >= arr[key]){
+    //             j--;
+    //         }
+    //         while(arr[i] <= arr[key] && i<j){
+    //             i++;
+    //         }
+            
+    //         if (i>=j){
+    //             int temp = arr[j];
+    //             arr[j] = arr[key];
+    //             arr[key] = temp;
+    //         }
+    //         else{
+    //             // System.out.println(arr[i] + " | " + arr[j]);
+    //             int temp = arr[j];
+    //             arr[j] = arr[i];
+    //             arr[i] = temp;
+    //         }
+    //         // System.out.println(i + " | " + j);
+    //     }
+    //     System.out.println(Arrays.toString(arr) + " | " + i + " | " + j);
+    //     quick_sort(arr, start, j-1);
+    //     quick_sort(arr, j+1, end);
+    // }
     
 }
